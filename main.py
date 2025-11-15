e831d960a2299d8296e36351a5c5ab4982ccd8ac
Anshu <anshulbidhuri9876@gmail.com>
2025-11-15 15:43:51 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from playwright.sync_api import sync_playwright
+import time
+
+with sync_playwright() as p:
+    browser = p.firefox.launch(headless=False)
+    page = browser.new_page().add_init_script
+    page.goto("https://playwright.dev/python/docs/intro")
+    time.sleep(5)
+print(2344)
+
+1.1
