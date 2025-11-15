19f6a2872280014c21b2807c633873b276b67c5e
Anshul-Bidhuri <113173533+Anshul-Bidhuri@users.noreply.github.com>
2025-11-15 16:09:56 +0530

    Add print statement for debugging

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from playwright.sync_api import sync_playwright
 import time
 
+print(1111)
 with sync_playwright() as p:
     browser = p.firefox.launch(headless=False)
     page = browser.new_page().add_init_script
